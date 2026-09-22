8b3afcd85c6299db641cdb3adc1fa8b91987c148
Roberto Alencar <rroberto.alencar@gmail.com>
2026-09-22 20:33:28 -0300

    implementando projeto

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import ClienteForm from "../features/cliente/page/ClienteForm";
 import ClientePage from "../features/cliente/page/ClientePage";
 import Home from "../features/home/page/Home";
 
@@ -14,6 +15,7 @@ export default function Router() {
                 <Route path="/" element={<Home />} />
 
                 <Route path="/cliente" element={<ClientePage />} />
+                <Route path="/cliente-form" element={<ClienteForm />} />
 
             </Routes>
 
